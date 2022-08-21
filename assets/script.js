f75bead30d0802bee99a6f3d6f6ca6535d0d10a2
Dayne Lalmond <dlalmondo@gmail.com>
2022-08-21 15:18:35 -0400

    additional styling

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,59 @@
 var start = document.querySelector("#startBtn");
 var container = document.querySelector(".container");
+var currentIndex = 0;
+var points = 0;
+var questions = [
+  {
+    text: "Question 1",
+    choices: ["✗", "✓", "✗", "✗"],
+    correct: "✓"
+  },
+  {
+    text: "Question 2",
+    choices: ["✗", "✗", "✗", "✓"],
+    correct: "✓"
+  },
+  {
+    text: "Question 3",
+    choices: ["✗", "✓", "✗", "✗"],
+    correct: "✓"
+  },
+  {
+    text: "Question 4",
+    choices: ["✓", "✗", "✗", "✗"],
+    correct: "✓"
+  },
+  {
+    text: "Question 5",
+    choices: ["✗", "✗", "✗", "✓"],
+    correct: "✓"
+  },
+  {
+    text: "Question 6",
+    choices: ["✗", "✗", "✓", "✗"],
+    correct: "✓"
+  },
+  {
+    text: "Question 7",
+    choices: ["✓", "✗", "✗", "✗"],
+    correct: "✓"
+  },
+  {
+    text: "Question 8",
+    choices: ["✗", "✓", "✗", "✗"],
+    correct: "✓"
+  },
+  {
+    text: "Question 9",
+    choices: ["✗", "✗", "✗", "✓"],
+    correct: "✓"
+  },
+  {
+    text: "Question 10",
+    choices: ["✗", "✗", "✓", "✗"],
+    correct: "✓"
+  }
+]
 
 var mode = "queued";
 
@@ -8,31 +62,20 @@ start.addEventListener("click", function() {
   if(mode === "queued") {
     start.setAttribute("class", "started");
 // the start variable's style is changed to ".started" which hides the button    
-// below are the actions to perform IF the mode is queued which it is by default
-    var answers = ["Remaru", "Veldora", "Gobbiru", "Miliem"];
-answers.forEach(function(i) {
-// forEach will allow a function to act upon each element of the array
-// the parameter (i) can only be called inside the function
-    const button = document.createElement("button");
-    button.innerText = i;
-// innerText is just displaying the value of the array called
-        button.addEventListener("click", function() {
-            document.getElementById("check").remove();
-            clearInterval(theTimer);
-// the function has the event listener inside because it needs
-// to be checking that the user clicks on the created button
-        })
-// finally it attaches the button to the check element
-    check.append(button);
-});
-document.getElementById("theSpan").innerHTML = "<strong>Here is the first question?</strong>"
+// below are the actions to perform IF the mode is "queued" which it is by default
+displayQuestion();
     var timeleft = 10;
     var theTimer = setInterval(function() {
   if(timeleft <= 0) {
 // additionally, a timer begins when the user clicks on start
 // the timer will remove the question at the end of its interval
+    const percentage = Math.round((points / 10) * 100);  
+    document.getElementById("theSpan").innerText = points + "/10 Correct"
+    document.getElementById("percentage").innerText = percentage + "%"
     document.getElementById("check").remove();
     clearInterval(theTimer);
+    // looks like this all I need to save points to the local storage!
+    localStorage["points"] = points
   }
   document.getElementById("timer").innerHTML = timeleft
   document.getElementById("timer").value = 10 - timeleft;
@@ -50,11 +93,40 @@ container.setAttribute("class", "active");
   };
 });
 
+// the current index starts at 0 and is incremented in the checkAnswers function
+
+function displayQuestion() {
+  document.getElementById("theSpan").innerHTML = questions[currentIndex].text
+  questions[currentIndex].choices.forEach(function(i) {
+    // forEach will allow a function to act upon each element of the array
+    // the parameter (i) can only be called inside the function
+        const button = document.createElement("button");
+        button.innerText = i;
+    // innerText is just displaying the value of the array called
+            button.addEventListener("click", checkAnswers)
+                // check answers function without () because it is not called immidiately
+    // the function has the event listener inside because it needs
+    // to be checking that the user clicks on the created button
+    // finally it attaches the button to the check element
+        check.append(button);
+    });
+}
 
 
+function checkAnswers(event)
+// Event is accepting the event object created from the button event listener
+{
+  //event.target is the element that was selected
+var result = (event.target.textContent);
+var asCorrect = questions[currentIndex].correct;
+if (result === asCorrect) {
+  points++
+}
 
-// ===========================================================
+else {}
 
-// for (var i = 0; i < questions.length; i++) {
-//   document.getElementById("check").setAttribute("style", "display: flex");
-// } 
+// check innerHTML clears the element
+  check.innerHTML = ""
+  currentIndex++
+  displayQuestion();
+}
